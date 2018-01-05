71861857f00d58a5400b8cecd5c8a7ad229c2830
vagrant <vagrant@homestead>
2018-01-05 03:19:48 +0000

    Finish layout and routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,16 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Sample App</title>
-  </head>
-  <body>
-    <h1>主页</h1>
-  </body>
-</html>
+@extends('layouts.default')
+
+@section('content')
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+     欢迎来到Snowman个人小站<a href="https://fsdhub.com/books/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页。
+    </p>
+    <p>
+        <h2 style="color:blue;"> 我叫刀疤涛，我在用Laravel框架，我在github等你！！！</h2>
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
+    </p>
+  </div>
+@stop
