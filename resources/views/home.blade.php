65440108a6fb7b032aa3912dc66e87c864ee250e
SnowmanNunu <345750542@qq.com>
2018-01-10 06:25:50 +0000

    master test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
     <p class="lead">
-     欢迎来到Snowman个人小站<a href="https://fsdhub.com/books/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页。
+     欢迎来到Snowman个人小站^_^ 0.0主分支master<a href="https://fsdhub.com/books/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页。
     </p>
     <p>
         <h2 style="color:blue;"> 我叫刀疤涛，我在用Laravel框架，我在github等你！！！</h2>
